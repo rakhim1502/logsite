80fe09f4af80bf729696cf98166ae756078fdee9
rakhim1502 <uzbrm95@gmail.com>
2026-08-05 23:39:00 +0500

    refactor: comment out minification in vite.config.js for build clarity

--- client/vite.config.js
+++ client/vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser',
+    // minify: 'terser',
     terserOptions: {
       compress: {
         drop_console: true,
